ff988019c2560c6a658fd68460cd71192c84bebd
jernejkokot <jernejkokot@gmail.com>
2014-07-04 14:41:39 +0200

    update

--- survey/Scripts/script.js
+++ survey/Scripts/script.js
@@ -8,7 +8,6 @@ function postContactToGoogle() {
     var Age = $('input[name="Age"]:checked').val();
     var Gender = $('input[name="Gender"]:checked').val();
     var Location = $('#Location').val();
-    var Occupation = $('input[name="Occupation"]:checked').val(); // work status
     var Email = $('#Email').val();
     var LastPage = $('#c5').hasClass('pt-page-current');
 
@@ -16,13 +15,12 @@ function postContactToGoogle() {
         url: "https://docs.google.com/forms/d/1R-b5cSkLV9zK4i8P5qyI3Mj9xyjTV0tUS1LLC04xbTw/formResponse",
         data: {
             "entry_1203314722": Language,
-            "entry_131733371": QuestionId,
+            "entry_185113739": QuestionId,
             "entry_131733371": Question,
             "entry_641039761": Answer,
             "entry_14591124": Age,
             "entry_336794213": Gender,
             "entry_2062063561": Location,
-            "entry_185113739": Occupation,
             "entry_950247682": Email
             
         },
